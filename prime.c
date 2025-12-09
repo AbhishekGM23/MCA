ae9bca2c3eef99839cdd1819f5e3971d0e716fdf
AbhishekGM23 <abhishekgm20@gmail.com>
2025-12-10 05:27:09 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,52 +1,61 @@
 #include <lpc214x.h>
 
-void init_pll(void){
-    PLL0CON = 0x01;     
-    PLL0CFG = 0x24;     
-    PLL0FEED = 0xAA;    
-    PLL0FEED = 0x55;    
-    while(!(PLL0STAT & (1<<10))); 
-    PLL0CON = 0x03;     
-    PLL0FEED = 0xAA;    
-    PLL0FEED = 0x55;    
-    VPBDIV = 0x01;      
+void inti_pll(void) {
+    PLL0CON = 0x01;
+    PLL0CFG = 0x24;
+    PLL0FEED = 0xAA;
+    PLL0FEED = 0x55;
+
+    while(!(PLL0STAT & (1<<10)));  // Wait for PLL lock
+
+    PLL0CON = 0x03;
+    PLL0FEED = 0xAA;
+    PLL0FEED = 0x55;
+
+    VPBDIV = 0x01;
 }
 
-void delay_ms(int ms){
-    T0PR = 60000 - 1;   
-    T0TCR = 0x02;       
+void delay_ms(int ms) {
+    T0PR = 60000 - 1;
+    T0TCR = 0x02;
     T0TC = 0;
     T0TCR = 0x01;
+    
     while(T0TC < ms);
     T0TCR = 0x00;
 }
 
-int main(){
-    int n = 17, i, isPrime = 1;
+int is_prime(int num) {
+    int i;
+    if(num <= 1) return 0;
+    for(i = 2; i * i <= num; i++) {
+        if(num % i == 0)
+            return 0;
+    }
+    return 1;
+}
 
-    init_pll();
+int main(void) {
+    int count = 0;
+    int number = 2;  // Start checking from 2 (first prime)
+
+    inti_pll();
     PINSEL0 = 0x00000000;
-    IO0DIR  = 0x000003FF; // LEDs on P0.0 - P0.9
+    IODIR0 |= 0x000003FF;  // P0.0–P0.9 as output
 
-    // Check if prime
-    if(n <= 1) isPrime = 0;
-    else {
-        for(i = 2; i <= n/2; i++) {
-            if(n % i == 0) {
-                isPrime = 0;
-                break;
+    while(1) {
+        count = 0;
+        number = 2;
+
+        while(count < 10) {       // First 10 prime numbers
+            if(is_prime(number)) {
+                IOSET0 = number;       // Display prime number in binary
+                delay_ms(400);
+                IOCLR0 = 0x000003FF;   // Clear LEDs
+                delay_ms(200);
+                count++;
             }
+            number++;
         }
     }
-
-    if(isPrime) {
-        IO0SET = 0x00000001;  // Turn ON LED on P0.0 for Prime
-    } else {
-        IO0SET = 0x00000002;  // Turn ON LED on P0.1 for Not Prime
-    }
-
-    delay_ms(2000);
-    IO0CLR = 0x000003FF;
-
-    while(1); // Stop here
 }
