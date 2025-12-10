ae7fd6ef19ce5accdf5c7c7adf53606a72753065
AbhishekGM23 <abhishekgm20@gmail.com>
2025-12-10 05:34:39 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,44 +1,45 @@
 #include <lpc214x.h>
 
-void init_pll(void){
-    PLL0CON = 0x01;     
-    PLL0CFG = 0x24;     
-    PLL0FEED = 0xAA;    
-    PLL0FEED = 0x55;    
-    while(!(PLL0STAT & (1<<10))); 
-    PLL0CON = 0x03;     
-    PLL0FEED = 0xAA;    
-    PLL0FEED = 0x55;    
-    VPBDIV = 0x01;      
+void inti_pll(void) {
+    PLL0CON = 0x01;
+    PLL0CFG = 0x24;
+    PLL0FEED = 0xAA;
+    PLL0FEED = 0x55;
+    while(!(PLL0STAT & (1<<10)));
+    PLL0CON = 0x03;
+    PLL0FEED = 0xAA;
+    PLL0FEED = 0x55;
+    VPBDIV = 0x01;
 }
 
-void delay_ms(int ms){
-    T0PR = 60000 - 1;   
-    T0TCR = 0x02;       
+void delay_ms(int ms) {
+    T0PR = 60000 - 1;
+    T0TCR = 0x02;
     T0TC = 0;
     T0TCR = 0x01;
     while(T0TC < ms);
     T0TCR = 0x00;
 }
 
-int main(){
-    unsigned int n = 5;
-    unsigned long long fact = 1;
-    int i;
+int main(void) {
+    int n = 6;   // upto 6! because LEDs can display max 720
+    int i, j, fact;
 
-    init_pll();
+    inti_pll();
     PINSEL0 = 0x00000000;
-    IO0DIR  = 0x000003FF; // LEDs on P0.0 - P0.9
+    IODIR0 |= 0x000003FF;
 
-    // Calculate factorial
-    for(i = 1; i <= n; i++) {
-        fact *= i;
+    while(1) {
+        for(i = 1; i <= n; i++) {
+
+            fact = 1;
+            for(j = 1; j <= i; j++)
+                fact *= j;   // factorial calculation
+
+            IOSET0 = fact;
+            delay_ms(300);
+            IOCLR0 = 0x000003FF;
+            delay_ms(150);
+        }
     }
-
-    // Display factorial value on LEDs (for simplicity, only lower 10 bits)
-    IO0SET = fact & 0x000003FF;
-    delay_ms(1000);
-    IO0CLR = 0x000003FF;
-
-    while(1); // Stop here
 }
